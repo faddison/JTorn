3f1ce2cea12cd792f8acd32f7a4e70506d964bfa
fraser <fraseraddison@gmail.com>
2012-10-07 15:48:49 -0700

    -added abort() method to handle all exit scenarios -got rid of excess exception handling -prefixed all log messages with bot's username (user.getUsername())

--- src/com/jtorn/bot/misc/TornBot.java
+++ src/com/jtorn/bot/misc/TornBot.java
@@ -3,6 +3,10 @@ package com.jtorn.bot.misc;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -14,6 +18,7 @@ import com.DeathByCaptcha.SocketClient;
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
 import com.jtorn.bot.CaptchaSolver;
+import com.jtorn.bot.TornProxy;
 import com.jtorn.bot.TornUser;
 
 public class TornBot implements Runnable
@@ -21,62 +26,72 @@ public class TornBot implements Runnable
 
 	private WebClient wc;
 	private TornUser user;
+	DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	
-	public TornBot(WebClient wc, TornUser user)
+	public TornBot(TornUser user)
 	{
-		this.wc = wc;
 		this.user = user;
+		TornProxy proxy = user.getProxy();
+		
+		wc = new WebClient(BrowserVersion.FIREFOX_10, 
+						   proxy.getAddress(), 
+						   Integer.parseInt(proxy.getPort()));
+	    final DefaultCredentialsProvider scp = new DefaultCredentialsProvider();
+	    scp.addCredentials(proxy.getUsername(), proxy.getPassword());
+	    wc.setCredentialsProvider(scp);
+	    // JavaScript throws major errors on site.
+		wc.setJavaScriptEnabled(false);
+		wc.setThrowExceptionOnScriptError(false);
+		wc.setThrowExceptionOnFailingStatusCode(false);
 	}
 	
 	public void run()
 	{
 		try
 		{
-			System.out.println("Starting application...");
+			System.out.println(this.user.getUsername()+"-"+"Starting application...");
 			java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-			
-			wc = new WebClient(BrowserVersion.FIREFOX_10, "50.117.67.126", 3131);
-		    final DefaultCredentialsProvider scp = new DefaultCredentialsProvider();
-		    scp.addCredentials("754d539bc6ade1ad", "454585f105747a76");
-		    wc.setCredentialsProvider(scp);
-			wc.setJavaScriptEnabled(false);
-			
+
 		    // Get the first page
 		    HtmlPage page = wc.getPage("http://torn.com/index.php");
 		    if (page.asText().contains("You are no longer logged in."))
-		    	page = login(page, "Bier", "nibodi70");
+		    	page = login(page, user.getUsername(), user.getPassword());
 		    else
-		    	System.out.println("Already logged in.");
+		    	System.out.println(this.user.getUsername()+"-"+"Already logged in.");
 		    	
-		    System.out.println("Index.php loaded.");
+		    System.out.println(this.user.getUsername()+"-"+"Index.php loaded.");
 	        int loop = 0;
 	        int errors = 0;
 		    while (true)
 		    {
 
-		    	System.out.println("Beginning loop "+loop+"...");
+		    	System.out.println(this.user.getUsername()+"-"+"Beginning loop "+loop+"...");
 		    	loop++;
 		    	try
 		    	{
 			    	if(inHospital(page) || inJail(page))
 			    	{
-			    		System.out.println("Sleeping 5 minutes...");	
+			    		System.out.println(this.user.getUsername()+"-"+"Sleeping 5 minutes...");	
 					    Thread.sleep(1000*5*60);
 			    	}
 			    	else
 			    	{
-			    		System.out.println("Loading gym...");
+			    		System.out.println(this.user.getUsername()+"-"+"Loading gym...");
 			    		page = wc.getPage("http://torn.com/gym.php");
 					    while (onCaptcha(page))
 					    {
 					    	wc.setJavaScriptEnabled(true);
-						    System.out.println("Captcha encountered.");
+						    System.out.println(this.user.getUsername()+"-"+"Captcha encountered.");
 						    page = solveCaptcha(page);
 						    wc.setJavaScriptEnabled(false);
 					    }
-					    //System.out.println(page.getWebResponse().getContentAsString());
+					    //System.out.println(this.user.getUsername()+"-"+page.getWebResponse().getContentAsString());
 					    trainStrength(page, 20);
-					    System.out.println("Sleeping 30 minutes...");
+					    //doCrime(page, 5);
+					    System.out.println(this.user.getUsername()+"-"+"Sleeping 30 minutes...");
+						Calendar cal = Calendar.getInstance();
+						cal.add(Calendar.MINUTE, 30);
+						System.out.println(this.user.getUsername()+"-"+"Waking at " + dateFormat.format(cal.getTime()));
 					    Thread.sleep(1000*30*60);
 					    
 				    }
@@ -84,35 +99,33 @@ public class TornBot implements Runnable
 		    	}
 		    	catch (Exception e)
 		    	{
-		    		System.out.println(e.toString());
+		    		System.out.println(this.user.getUsername()+"-"+e.toString());
+		    		e.printStackTrace();
 		    		errors++;
 		    		page = loadIndex();
 		    		if (errors > 3)
-		    			System.exit(1);
+		    			abort();
 		    	}
 		    }
 		    
 		}
 		catch (Exception e)
 		{
-			wc.closeAllWindows();
+			System.out.println(this.user.getUsername()+"-"+e.toString());
 			e.printStackTrace();
+			abort();
 		}
 	}
 		
 	public HtmlPage loadIndex()
 	{
-		System.out.println("Loading Index...");
-		try {
+		System.out.println(this.user.getUsername()+"-"+"Loading Index...");
+		try 
+		{
 			return wc.getPage("http://torn.com/index.php");
-		} catch (FailingHttpStatusCodeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} 
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 		return null;
@@ -125,7 +138,7 @@ public class TornBot implements Runnable
 			return false;
 	    boolean result = e.getAttribute("title").toLowerCase().contains("hospital");
 	    if (result)
-	    	System.out.println("In hospital.");
+	    	System.out.println(this.user.getUsername()+"-"+"In hospital.");
 	    return result;
 	}
 	
@@ -133,7 +146,7 @@ public class TornBot implements Runnable
 	{
 		boolean result = (page.getElementById("icon16") != null);
 		if (result)
-			System.out.println("In jail.");
+			System.out.println(this.user.getUsername()+"-"+"In jail.");
 		return result;
 		
 	}
@@ -161,36 +174,23 @@ public class TornBot implements Runnable
 			try
 			{
 				if (attempts > 3)
-					System.exit(1);
+					abort();
 				attempts++;
-				System.out.println("Logging in... (Attempt "+attempts+")");
-				
+				System.out.println(this.user.getUsername()+"-"+"Logging in... (Attempt "+attempts+")");
 				page = wc.getPage("http://torn.com/login");
-			    
 			    final HtmlForm form = page.getFormByName("login");
-		
 			    HtmlSubmitInput button = form.getInputByName("btnLogin");
-			    
 			    HtmlTextInput textField = form.getInputByName("player");
-		
-			    // Change the value of the text field
 			    textField.setValueAttribute(username);
-			    
 			    final HtmlPasswordInput passwordField = form.getInputByName("password");
-				
-			    // Change the value of the text field
 			    passwordField.setValueAttribute(password);
-		
-			    // Now submit the form by clicking the button and get back the second page.
 			    page = button.click();
-			    
 			    page = wc.getPage("http://torn.com/index.php");
 			    loggedIn = true;
-			    
-			}
+		    }
 			catch (Exception e)
 			{
-				System.out.println(e.toString());
+				System.out.println(this.user.getUsername()+"-"+e.toString());
 				loggedIn = false;
 			}
 		}
@@ -200,7 +200,7 @@ public class TornBot implements Runnable
 	
 	public HtmlPage trainStrength(HtmlPage page, int amount) throws IOException
 	{
-		System.out.println("Training "+amount+" strength...");
+		System.out.println(this.user.getUsername()+"-"+"Training "+amount+" strength...");
 		HtmlTextInput amountInput = (HtmlTextInput) page.getByXPath("/html/body/div[4]/table/tbody/tr/td[2]/center/div[2]/div/div[2]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr/td[1]/input[1]").get(0);
 		amountInput.setValueAttribute(Integer.toString(amount));
 		HtmlElement e = (HtmlElement) page.getByXPath("/html/body/div[4]/table/tbody/tr/td[2]/center/div[2]/div/div[2]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr/td[3]/input").get(0);
@@ -210,7 +210,7 @@ public class TornBot implements Runnable
 	
 	private HtmlPage solveCaptcha(HtmlPage page) throws Exception
 	{
-		System.out.println("Solving captcha...");
+		System.out.println(this.user.getUsername()+"-"+"Solving captcha...");
 		String url = page.getUrl().toString();
 		page = wc.getPage(url+"?bypass=1");
 		
@@ -225,9 +225,15 @@ public class TornBot implements Runnable
 	    textField.setValueAttribute(captcha.text);
 	    HtmlElement element = (HtmlElement)page.getElementByName("submit");
 	    page = element.click();
-	    System.out.println("Captcha entered.");
+	    System.out.println(this.user.getUsername()+"-"+"Captcha entered.");
 	    return wc.getPage(url);
-	    
+	}
+	
+	public void abort()
+	{
+		System.out.println(this.user.getUsername()+"-"+"Too many errors encountered. Aborting all operations");
+		wc.closeAllWindows();
+		System.exit(1);
 	}
 }
 
