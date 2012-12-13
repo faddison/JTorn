cd729875e78b2878d2777249936ac7bb4d028eb0
fraser <fraseraddison@gmail.com>
2012-12-13 00:35:27 -0800

    added dbc captcha class

---
+++ src/com/jtorn/util/TornUtil.java
@@ -0,0 +1,24 @@
+package com.jtorn.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.jtorn.bot.TornConstants;
+
+public class TornUtil
+{
+
+	public static String getTime(String stringFormat)
+	{
+		return getTime(stringFormat, 0);
+	}
+	
+	public static String getTime(String stringFormat, int minutes)
+	{
+		Calendar futureTime = Calendar.getInstance();
+		futureTime.add(Calendar.MINUTE, minutes);
+		SimpleDateFormat dateFormat = new SimpleDateFormat(stringFormat);
+		return dateFormat.format(futureTime.getTime());
+	}
+}
